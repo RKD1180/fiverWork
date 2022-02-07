28826ad4184cb033e3aa44c812f5f0917da92c86
1emonislam <emonislamweb@gmail.com>
2022-02-07 19:57:19 +0600

    redirects add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./componnent/Home/Home";
 
 function App() {
@@ -6,6 +6,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="*" element={<><h2>Page Not found 404!</h2></>}></Route>
+
       </Routes>
     </BrowserRouter>
   );
